de3a2158e1ef0d409fb278f8e651b9bc550f78ba
Joky <jokyxray@gmail.com>
2020-09-09 23:29:29 +0800

    ss

--- app/src/main/java/com/sunward/test/MainActivity.kt
+++ app/src/main/java/com/sunward/test/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.sunward.test
 
 import android.content.Context
-import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
+import android.util.MutableBoolean
 import androidx.appcompat.app.AppCompatActivity
-import com.baidu.mapapi.map.FileTileProvider
-import com.baidu.mapapi.map.Tile
-import com.baidu.mapapi.map.TileOverlayOptions
+import com.baidu.mapapi.map.*
 import com.baidu.mapapi.model.LatLng
 import com.baidu.mapapi.model.LatLngBounds
+import com.baidu.mapapi.model.inner.Point
 import kotlinx.android.synthetic.main.activity_main.*
 import mapsdkvi.com.gdi.bgl.android.java.EnvDrawText.bmp
 import java.io.IOException
@@ -24,8 +24,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        bmapView.map.mapType = BaiduMap.MAP_TYPE_NONE
+        // 获取/assets目录下的所有文件
+        val images = assets.list("output/3/0");
+        Log.e("ss","ss")
+//        bmapView.map.mapType = BaiduMap.MAP_TYPE_NONE
         /**
          * 定义瓦片图的离线Provider，并实现相关接口
          * MAX_LEVEL、MIN_LEVEL 表示地图显示瓦片图的最大、最小级别
@@ -39,9 +42,13 @@ class MainActivity : AppCompatActivity() {
         val tileProvider = object : FileTileProvider() {
             override fun getTile(x: Int, y: Int, z: Int): Tile {
                 // 根据地图某一状态下x、y、z加载指定的瓦片图
-                val filedir = "output/$z/$x/$y/.jpg"
+                val filedir = "output/3/0/0.jpg"
                 //将瓦片图资源解析为Bitmap
-                val bm: Bitmap = getBitmapFromAsset(this@MainActivity, filedir) ?: return Tile(256, 256, ByteArray(256*256))
+                val bm: Bitmap = getBitmapFromAsset(this@MainActivity, filedir) ?: return Tile(
+                    256, 256, ByteArray(
+                        256 * 256
+                    )
+                )
                 // 通过瓦片图bitmap构造Tile示例
                 val offlineTile = Tile(bm.width, bm.height, toRawData(bm))
                 bm.recycle()
@@ -49,11 +56,11 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun getMaxDisLevel(): Int {
-                return 4
+                return 21
             }
 
             override fun getMinDisLevel(): Int {
-                return 2
+                return 3
             }
         }
 
@@ -63,38 +70,45 @@ class MainActivity : AppCompatActivity() {
         val options = TileOverlayOptions()
 // 构造显示瓦片图范围，当前为世界范围
 // 构造显示瓦片图范围，当前为世界范围
-        val northeast = LatLng(113.189392090,28.259582520)
-        val southwest = LatLng(113.171539307,28.245849609)
+        val northeast = LatLng(28.259582520,113.189392090)
+        val southwest = LatLng(28.245849609,113.171539307)
+//        val northeast = LatLng(80.0, 180.0)
+//        val southwest = LatLng(-80.0, -180.0)
         options.tileProvider(tileProvider)
-                .setPositionFromBounds(LatLngBounds.Builder().include(northeast).include(southwest).build())
+                .setPositionFromBounds(
+                    LatLngBounds.Builder().include(northeast).include(southwest).build()
+                )
 //向地图添加离线瓦片图对象
 //向地图添加离线瓦片图对象
 
         val tileOverlay = bmapView.map.addTileLayer(options)
+        Log.e("ss", "ss")
+        bmapView.map.setMapStatus(MapStatusUpdateFactory.newLatLngBounds(LatLngBounds.Builder().include(northeast).include(southwest).build()));
     }
 
     fun toRawData(bm: Bitmap): ByteArray? {
-        val size = bmp.rowBytes * bmp.height
+        val size = bm.rowBytes * bm.height
         val b: ByteBuffer = ByteBuffer.allocate(size)
 
-        bmp.copyPixelsToBuffer(b)
+        bm.copyPixelsToBuffer(b)
 
         val bytes = ByteArray(size)
 
         try {
-            b.get(bytes, 0, bytes.size)
+            return b.array()
+//            b.get(bytes, 0, bytes.size)
         } catch (e: BufferUnderflowException) {
+            e.printStackTrace()
             return null
         }
         return bytes
     }
 
     fun getBitmapFromAsset(context: Context, filePath: String?): Bitmap? {
-        val assetManager: AssetManager = context.getAssets()
         val istr: InputStream
         var bitmap: Bitmap? = null
         try {
-            istr = assetManager.open(filePath!!)
+            istr = assets.open(filePath!!)
             bitmap = BitmapFactory.decodeStream(istr)
         } catch (e: IOException) {
             // handle exception
